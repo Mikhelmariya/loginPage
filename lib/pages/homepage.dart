b3ceb5bd50c3feb8a5ef4a1cb8f317395ff07429
Mikhelmariya <mikhela65@gmail.com>
2023-03-08 22:12:04 +0530

    added email with welcome message

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,19 +1,33 @@
+import 'package:auth/auth.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 import 'package:login/loginscreen.dart';
 
 class Homepage extends StatelessWidget {
-  const Homepage({super.key});
+  Homepage({super.key});
+  void signout() {
+    FirebaseAuth.instance.signOut();
+  }
 
+  final user = FirebaseAuth.instance.currentUser;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-          child: Text(
-        "Welcome mikhel",
-        style: TextStyle(fontSize: 25),
-      )),
+    return Scaffold(
+      appBar: AppBar(actions: [
+        IconButton(
+            onPressed: () {
+              signout();
+            },
+            icon: Icon(Icons.logout))
+      ]),
+      body: Container(
+        child: Center(
+            child: Text(
+          "Welcome " + user!.email!,
+          style: TextStyle(fontSize: 25),
+        )),
+      ),
     );
   }
 }
